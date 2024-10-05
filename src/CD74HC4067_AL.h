709a5f3aa9a8585985182684cad452c6308000a0
DwiYI <ilhamyanuar65@gmail.com>
2024-10-05 10:28:59 +0700

    Update and rename CD74HC4067.h to CD74HC4067_AL.h

--- src/CD74HC4067_AL.h
+++ src/CD74HC4067_AL.h
@@ -4,18 +4,18 @@
   LGPL 3.0
 */
 
-#ifndef CD74HC4067_h
-#define CD74HC4067_h
+#ifndef CD74HC4067_AL_h
+#define CD74HC4067_AL_h
 
 #include "Arduino.h"
 #include <inttypes.h>
 
 extern uint8_t _g_channel_truth_table[16][4];
 
-class CD74HC4067
+class CD74HC4067_AL
 {
   public:
-    CD74HC4067(uint8_t s0, uint8_t s1, uint8_t s2, uint8_t s3);
+    CD74HC4067_AL(uint8_t s0, uint8_t s1, uint8_t s2, uint8_t s3);
     void channel(uint8_t channel);
   private:
   	uint8_t _s0;
